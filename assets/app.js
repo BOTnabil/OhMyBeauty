1236410a3f0b05586fccbd07b43daf2b08377077
Nabil <l4teoyt@gmail.com>
2024-07-30 19:45:12 +0200

    Ajout de vue accueil mais ne fonctionne pas encore

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,21 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
+// Fonctionnement du menu burger
+
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+function openNav(){
+    sidenav.classList.add("active");
+}
+
+function closeNav(){
+    sidenav.classList.remove("active");
+}
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
