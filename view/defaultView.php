155f39b638cf0f5f31e02a31098bb2568a38b847
Nabil <l4teoyt@gmail.com>
2024-08-08 17:03:01 +0200

    Correction de bugs responsive + amelioration de la section contact et services

--- view/defaultView.php
+++ view/defaultView.php
@@ -6,14 +6,14 @@ ob_start();
     <p class="citation">BIENVENUE CHEZ OH MY BEAUTY, L'INSTITUT QUI SAIT PRENDRE SOIN DE VOUS</p>
 </section>
 
-<section class="galerie">
+<section class="services">
     <a href="#" class="link1-4" ><img class="img1-4" src="./public/img/service_1.jpg" alt=""><img class="plus1" src="./public/img/plus-symbol.png" alt="symbole plus"></a>
     <a href="#" class="link1-4" ><img class="img1-4" src="./public/img/service_2.jpg" alt=""><img class="plus2" src="./public/img/plus-symbol.png" alt="symbole plus"></a>
     <a href="#" class="link1-4" ><img class="img1-4" src="./public/img/service_3.jpg" alt=""><img class="plus3" src="./public/img/plus-symbol.png" alt="symbole plus"></a>
     <a href="#" class="link1-4" ><img class="img1-4" src="./public/img/service_4.jpg" alt=""><img class="plus4" src="./public/img/plus-symbol.png" alt="symbole plus"></a>   
 </section>
 
-<section class="tradition1">
+<section class="aPropos1">
     <div class="boxLeftTop">
         <h1>UN SAVOIR FAIRE</h1>
         <h2>Un mot d'ordre : "qualifié"</h2>
@@ -21,10 +21,11 @@ ob_start();
         consectetur iste sunt, commodi quis veritatis laboriosam ex? </p>
         <a href="">EN SAVOIR PLUS</a>
     </div>
-    <img src="./public/img/a_propos_1.png" class="logoBeerTradition" alt="">
+    <img src="./public/img/a_propos_1.png" class="imgOMBaPropos1" alt="">
 </section>
-<section class="tradition2">
-    <img src="./public/img/a_propos_2.png" class="logoPubStory" alt="">
+
+<section class="aPropos2">
+    <img src="./public/img/a_propos_2.png" class="imgOMBaPropos2" alt="">
     <div class="boxRightBot">
         <h1>NOTRE HISTOIRE</h1>
         <h2>Oh My Beauty, un rêve devenu réalité</h2>
@@ -34,12 +35,22 @@ ob_start();
     </div>
 </section>
 
-<section class="new section">
-    
-</section>
-
-<section class="parallaxe">
-    <h2>THIS IS OUR OFFER</h2>
+<section class="contact">
+    <h2>CONTACTEZ NOUS</h2>
+    <div class="logos">
+        <div class="logos-footer">
+            <a href="#"><i class="fa-brands fa-facebook-f"></i> </a>
+        </div>
+        <div class="logos-footer">
+            <a href="#"><i class="fa-brands fa-x-twitter"></i> </a>
+        </div>
+        <div class="logos-footer">
+        <a href="#"> <i class="fa-brands fa-tiktok"></i> </a>
+        </div>
+        <div class="logos-footer">
+        <a href="#"> <i class="fa-solid fa-p"></i> </a>
+        </div>
+    </div>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium deleniti magni placeat dolorum</p>
 </section>
 
