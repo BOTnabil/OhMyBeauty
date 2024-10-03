6271929f912330c25bbeaa6688d70d4672cbd96f
Nabil <l4teoyt@gmail.com>
2024-10-03 15:50:48 +0200

    MAJ BDD

--- ohmybeautyBDD.sql
+++ ohmybeautyBDD.sql
@@ -38,17 +38,17 @@ INSERT INTO `categorie` (`id_categorie`, `designation`) VALUES
 CREATE TABLE IF NOT EXISTS `commande` (
   `id_commande` int NOT NULL AUTO_INCREMENT,
   `numeroCommande` bigint DEFAULT NULL,
-  `dateCommande` date NOT NULL,
+  `dateCommande` datetime NOT NULL,
   `prixTotal` decimal(15,2) NOT NULL,
-  `id_utilisateur` int NOT NULL,
+  `id_utilisateur` int DEFAULT NULL,
   `infosCommande` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id_commande`),
   UNIQUE KEY `numeroCommande` (`numeroCommande`),
   KEY `id_utilisateur` (`id_utilisateur`),
   CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table ohmybeauty.commande : ~0 rows (environ)
+-- Listage des données de la table ohmybeauty.commande : ~1 rows (environ)
 DELETE FROM `commande`;
 
 -- Listage de la structure de table ohmybeauty. contenir
@@ -62,10 +62,15 @@ CREATE TABLE IF NOT EXISTS `contenir` (
   KEY `contenir_ibfk_1` (`id_commande`),
   CONSTRAINT `contenir_ibfk_1` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id_commande`),
   CONSTRAINT `contenir_ibfk_2` FOREIGN KEY (`id_produit`) REFERENCES `produit` (`id_produit`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table ohmybeauty.contenir : ~0 rows (environ)
 DELETE FROM `contenir`;
+INSERT INTO `contenir` (`id_contenir`, `id_commande`, `id_produit`, `quantite`) VALUES
+	(1, NULL, 5, 1),
+	(2, NULL, 20, 5),
+	(3, NULL, 23, 5),
+	(4, NULL, 21, 6);
 
 -- Listage de la structure de table ohmybeauty. prestation
 CREATE TABLE IF NOT EXISTS `prestation` (
@@ -80,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `prestation` (
   CONSTRAINT `prestation_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`)
 ) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table ohmybeauty.prestation : ~30 rows (environ)
+-- Listage des données de la table ohmybeauty.prestation : ~7 rows (environ)
 DELETE FROM `prestation`;
 INSERT INTO `prestation` (`id_prestation`, `designation`, `prix`, `duree`, `id_categorie`, `description`) VALUES
 	(1, 'Réhaussement', 35.00, '45min', 1, 'test description'),
@@ -107,8 +112,7 @@ INSERT INTO `prestation` (`id_prestation`, `designation`, `prix`, `duree`, `id_c
 	(22, 'Soin visage éclat', 80.00, '60min', 3, 'Soin pour redonner de l’éclat à la peau'),
 	(23, 'Manucure classique', 30.00, '45min', 4, 'Soin des mains avec pose de vernis classique'),
 	(24, 'Pédicure spa', 45.00, '60min', 4, 'Soin des pieds avec bain et massage'),
-	(25, 'Coloration cheveux', 70.00, '60min', 2, 'Coloration complète avec produits de soin'),
-	(26, 'Balayage capillaire', 100.00, '60min', 2, 'Technique de coloration partielle des cheveux'),
+	(25, 'Coloration cheveux', 70.02, '60min', 2, 'Coloration compl&egrave;te avec produits de soin'),
 	(27, 'Soin contour des yeux', 50.00, '30min', 3, 'Soin ciblé pour le contour des yeux'),
 	(28, 'Epilation sourcils', 15.00, '15min', 1, 'Epilation des sourcils à la cire ou à la pince'),
 	(29, 'Pose de faux cils', 80.00, '60min', 1, 'Pose de faux cils semi-permanents'),
@@ -125,20 +129,18 @@ CREATE TABLE IF NOT EXISTS `produit` (
   PRIMARY KEY (`id_produit`),
   KEY `id_categorie` (`id_categorie`),
   CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table ohmybeauty.produit : ~26 rows (environ)
+-- Listage des données de la table ohmybeauty.produit : ~0 rows (environ)
 DELETE FROM `produit`;
 INSERT INTO `produit` (`id_produit`, `designation`, `prix`, `image`, `description`, `id_categorie`) VALUES
 	(1, 'Lime à ongles', 4.29, 'a_propos_2.png', 'Lorem ipsum truc bidule', 4),
-	(2, 'Kit de manucure', 9.50, NULL, 'Lorem ipsum truc bidule', 4),
 	(3, 'Kit vernis semi permanent', 24.99, NULL, 'Lorem ipsum truc bidule', 4),
 	(4, 'Ponceuse', 49.99, NULL, 'Lorem ipsum truc bidule', 4),
 	(5, 'Kit extension de cils', 11.49, NULL, 'Lorem ipsum truc bidule', 1),
 	(6, 'Faux cils (10 paires)', 8.99, NULL, 'Lorem ipsum truc bidule', 1),
 	(7, 'Sérum croissance', 22.99, NULL, 'Lorem ipsum truc bidule', 1),
 	(8, 'Faux cils magnétiques', 9.99, NULL, 'Lorem ipsum truc bidule', 1),
-	(9, 'Coffret skin care', 29.95, NULL, 'Lorem ipsum truc bidule', 3),
 	(10, 'Serre tête skin care', 2.00, NULL, 'Lorem ipsum truc bidule', 3),
 	(11, 'Masque au charbon', 4.50, NULL, 'Lorem ipsum truc bidule', 3),
 	(12, 'Masque hydratant', 3.50, NULL, 'Lorem ipsum truc bidule', 3),
@@ -146,7 +148,6 @@ INSERT INTO `produit` (`id_produit`, `designation`, `prix`, `image`, `descriptio
 	(14, 'Kit lissage brésilien', 15.90, NULL, 'Lorem ipsum truc bidule', 2),
 	(15, 'Bonnet en satin', 7.99, NULL, 'Lorem ipsum truc bidule', 2),
 	(16, 'Elastiques (20 pièces)', 4.00, NULL, 'Lorem ipsum truc bidule', 2),
-	(17, 'Shampooing nourrissant', 12.50, NULL, 'Shampooing pour cheveux secs et abîmés', 2),
 	(18, 'Crème hydratante visage', 25.00, NULL, 'Crème légère pour hydratation quotidienne', 3),
 	(19, 'Huile capillaire', 15.90, NULL, 'Huile réparatrice pour cheveux', 2),
 	(20, 'Vernis à ongles', 6.50, NULL, 'Vernis longue tenue, disponible en plusieurs couleurs', 4),
@@ -154,7 +155,6 @@ INSERT INTO `produit` (`id_produit`, `designation`, `prix`, `image`, `descriptio
 	(22, 'Poudre à sourcils', 11.99, NULL, 'Poudre pour maquillage des sourcils', 1),
 	(23, 'Crème anti-âge', 45.00, NULL, 'Crème pour réduire les signes de l’âge', 3),
 	(24, 'Sérum vitaminé', 19.99, NULL, 'Sérum enrichi en vitamines pour la peau', 3),
-	(25, 'Lotion tonique', 7.50, NULL, 'Lotion tonifiante pour le visage', 3),
 	(26, 'Lait corporel', 13.90, NULL, 'Lait hydratant pour le corps', 3);
 
 -- Listage de la structure de table ohmybeauty. reservation
@@ -181,12 +181,12 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `motDePasse` varchar(255) NOT NULL,
   `role` varchar(50) NOT NULL,
   PRIMARY KEY (`id_utilisateur`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table ohmybeauty.utilisateur : ~1 rows (environ)
 DELETE FROM `utilisateur`;
 INSERT INTO `utilisateur` (`id_utilisateur`, `email`, `motDePasse`, `role`) VALUES
-	(1, 'assatour.nabil@gmail.com', '$2y$10$lmhrwZ9L74IoWPEiU.UjuOu41klSUxVfMMcWiCVtQ5jccA20I3DDm', 'ADMIN');
+	(2, 'test@gmail.com', '$2y$10$l53hSsEXg0Tju0Lw5/wN2.NqARpGVEHqfypBPyvXuRmgvJpiZU35y', 'ADMIN');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
